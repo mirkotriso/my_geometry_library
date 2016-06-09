b264dbf001d87a707de261f05d33447684c3a554
mirkotriso <m.trisolini@soton.ac.uk>
2016-06-09 19:38:09 +0100

    Added 2D and 3D point classes

--- dev/point.py
+++ dev/point.py
@@ -1,5 +1,21 @@
-class Point(object):
+class Point2D(object):
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.coords = (x, y)
+    def __repr__(self):
+        return 'Point2D(%s, %s)' % self.coords
+    def __add__(self, p):
+        return Point2D(self.x + p.x, self.y + p.y)
+
+
+class Point3D(object):
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+		self.coords = (x, y, z)
+    def __repr__(self):
+        return 'Point3D(%s, %s, %s)' % self.coords
+    def __add__(self, p):
+        return Point3D(self.x + p.x, self.y + p.y, self.z + p.z)
